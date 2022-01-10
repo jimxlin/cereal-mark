1dfa4b7c8e04b5d81c960cc3d7f8f99f28424c17
Jim X. Lin <miljinx@gmail.com>
2022-01-09 22:22:41 -0800

    Remove cra assets

--- src/App.tsx
+++ src/App.tsx
@@ -179,25 +179,6 @@ function App() {
         </div>
       )}
       {error && <h1 style={{ color: "#f00" }}>ERROR: {error}</h1>}
-      {isLoading && (
-        <header
-          className="App-header"
-          style={{ display: isLoading ? "flex" : "none" }}
-        >
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      )}
       {isLoading && <Loading />}
       <SetErrorContext.Provider value={setError}>
         {!isLoading && seriesItems ? (
