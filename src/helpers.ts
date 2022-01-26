9b31e8a6994b2267e54c69b9fd31c8f9057e502c
Jim X. Lin <miljinx@gmail.com>
2022-01-25 20:54:51 -0800

    Truncate hash used for collection id

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,10 +1,11 @@
 import { sha256 } from "hash-wasm";
 
-export const generateId = (): Promise<string> => {
+export const generateId = async (): Promise<string> => {
   let uniqueId: string = Date.now().toString();
   uniqueId = uniqueId + navigator.userAgent;
   uniqueId = uniqueId + Math.floor(Math.random() * 1000).toString();
-  return sha256(uniqueId);
+  const hash = await sha256(uniqueId);
+  return hash.slice(31);
 };
 
 export const toUndefined = (value: any): any => {
